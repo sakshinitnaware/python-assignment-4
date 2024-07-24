59c767b380850400ef38d2670b26c44f65f9b426
sakshinitnaware <86613848+sakshinitnaware@users.noreply.github.com>
2024-07-24 17:15:50 +0530

    Update class_private_vari.py
    corrected the private value

--- class_private_vari.py
+++ class_private_vari.py
@@ -1,17 +1,16 @@
 # Define a class named Circle
 class Circle :
     # class private variable pi and its value
-    pi_circle = 3.141
+    __pi_circle = 3.141
     # Constructor method that initializes the Circle object with a list provided as an argument
     def __init__(self,radius):
         # Initialize the instance variable 'radius' with the radius
         self.radius = radius
-    # Method named 'area' that returns the instance variable 'radius'
+    # Method named 'area' that returns the area accessing the private variable pi'
     def area(self):
-        return self.radii
+        area = self.__pi_circle * self.radius**2 
+        return area
 # Create an instance of the Circle class with the instance variable value of radius as 5   
 a = Circle(5)
 # Call the 'area' method on the instance 'a' and print the returned radius
-print("radii :",a.area())
-# Call and print the pi value with the instance 'a'
-print("pi value :",a.pi_circle)
+print("radius :",a.area())
